143de5355d870b08d4f0ff35ab107cdbb06c4def
Parigya <parigyaj@gmail.com>
2020-07-19 00:43:11 +0530

    store the names and comments in the datastore

--- portfolio/src/main/java/com/google/sps/servlets/DataServlet.java
+++ portfolio/src/main/java/com/google/sps/servlets/DataServlet.java
@@ -18,6 +18,9 @@ import java.io.IOException;
 import com.google.gson.Gson;
 import java.util.ArrayList;
 import java.util.List;
+import com.google.appengine.api.datastore.DatastoreService;
+import com.google.appengine.api.datastore.DatastoreServiceFactory;
+import com.google.appengine.api.datastore.Entity;
 import com.google.sps.data.Comments;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -41,9 +44,12 @@ public class DataServlet extends HttpServlet {
   public void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException {
       String comment = getParameter(request, "comment-input", "");
       String name = getParameter(request, "name", "");
-      response.setContentType("text/html");
-      response.getWriter().println(name);
-      response.getWriter().println(comment);
+      Entity commentEntity = new Entity("Comments");
+      commentEntity.setProperty("name",name);
+      commentEntity.setProperty("comment",comment);
+      DatastoreService datastore = DatastoreServiceFactory.getDatastoreService();
+      datastore.put(commentEntity);
+      response.sendRedirect("/index.html");
   }
   private String getParameter(HttpServletRequest request, String name, String defaultValue){
       String value = request.getParameter(name);
